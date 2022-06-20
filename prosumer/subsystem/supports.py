265f9a136b9dcf16bc1a52eee96a75797e9c3937
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-20 21:31:30 +0530

    supports profile add get value at time

--- prosumer/subsystem/supports.py
+++ prosumer/subsystem/supports.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from functools import cached_property
+
+from utils.interpolate import Curves, remap
+
+
 class SupportsExport:
     export_price: float
     """
@@ -27,3 +33,23 @@ class SupportsRunProfile:
     def __init__(self, **kwargs) -> None:
         self.profile = list(map(float, kwargs.pop("profile").split(",")))
         super().__init__(**kwargs)
+
+    @cached_property
+    def profile_interval(self):
+        """
+        Returns the interval in seconds between each point in the predefined
+        profile.
+        """
+        return 24 * 60 * 60 / len(self.profile)
+
+    def get_value_for(self, time: datetime):
+        """
+        Evaluates the system's generic profile value for the specified time.
+        """
+        delta = (time - self.start_date).seconds
+        #               current time
+        # ------------|------*-----|------------>
+        #             i      x    i+1
+        i = delta // self.profile_interval
+        x_minus_i = (delta / self.profile_interval) % 1
+        return remap(Curves.sine(x_minus_i), 0, 1, *self.profile[i : i + 2])
