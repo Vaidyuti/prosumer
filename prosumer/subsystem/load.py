659cb95cc3bd4d907be1d64800c5efbec668241a
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-20 17:58:41 +0530

    implement basic load

--- prosumer/subsystem/load.py
+++ prosumer/subsystem/load.py
@@ -1,5 +1,12 @@
 from prosumer.subsystem.base import SubsystemBase
+from prosumer.subsystem.supports import SupportsRunProfile
 
 
-class Load(SubsystemBase):
-    pass
+class Load(SubsystemBase, SupportsRunProfile):
+
+    # Dynamic properties
+    net_consumption = 0.0
+    """The net consumption by the system in kW at present."""
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
