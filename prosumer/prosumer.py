cf2474d150cb28938c8c99d5cd1f821347e2881a
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-20 16:16:04 +0530

    implements prosumer base

---
+++ prosumer/prosumer.py
@@ -0,0 +1,19 @@
+from prosumer.settings import PROFILE
+from prosumer.subsystem.base import SubsystemBase
+from prosumer.subsystem import Generation, Load, Storage
+
+
+class Prosumer(SubsystemBase):
+    def __init__(
+        self,
+        vp_address: str = PROFILE["vp_address"],
+        location: str = PROFILE["location"],
+        generations: list = PROFILE["generations"],
+        loads: list = PROFILE["loads"],
+        storages: list = PROFILE["storages"],
+    ) -> None:
+        self.vp_address = vp_address
+        self.location = location
+        self.generations: list[Generation] = list(map(Generation, generations))
+        self.loads: list[Load] = list(map(Load, loads))
+        self.storages: list[Storage] = list(map(Storage, storages))
