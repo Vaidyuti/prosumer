24526cbdc8183dbc31c74e0565ea8e8d258b589d
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-20 17:58:24 +0530

    implement basic generation

--- prosumer/subsystem/generation.py
+++ prosumer/subsystem/generation.py
@@ -1,5 +1,26 @@
+from typing import Optional
 from prosumer.subsystem.base import SubsystemBase
+from prosumer.subsystem.supports import (
+    SupportsExport,
+    SupportsRunProfile,
+    SupportsTechnologyTag,
+)
 
 
-class Generation(SubsystemBase):
-    pass
+class Generation(
+    SubsystemBase, SupportsRunProfile, SupportsTechnologyTag, SupportsExport
+):
+
+    # Static properties
+    installed_kw: float
+    """
+    The installed generating capacity in kW. (i.e. the maximum output power this
+    system can yield in ideal conditions).
+    """
+
+    # Dynamic properties
+    net_generation = 0.0
+    """The net generation by the system in kW at present."""
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
