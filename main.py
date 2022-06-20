a5653d283773020cdd81985a49d44db8518c3c07
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-20 17:58:47 +0530

    update entrypoint

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
-import os
-import requests
-import yaml
 
-
-def fetch_profile() -> str:
-    res = requests.get(os.environ["PROFILE_ENDPOINT"])
-    return yaml.safe_load(res.text)
+from prosumer.prosumer import Prosumer
 
 
 if __name__ == "__main__":
-    profile = fetch_profile()
-    # Condition Profile
-    # Run profile
-    pass
+    prosumer = Prosumer()
+    prosumer.start()
