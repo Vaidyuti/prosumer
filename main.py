429e17025ee5c37f46c678de834e4fb4fa8ada08
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-20 18:30:40 +0530

    suspends program instead of exiting after start

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
+import time
 from prosumer.prosumer import Prosumer
+
+
+def suspend():
+    while True:
+        time.sleep(100)
 
 
 if __name__ == "__main__":
     prosumer = Prosumer()
     prosumer.start()
+    suspend()
