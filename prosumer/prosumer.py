21041afb0ffabbb7542e2c5c16e2146346889022
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-21 11:06:42 +0530

    implements required attributes of prosumer

--- prosumer/prosumer.py
+++ prosumer/prosumer.py
@@ -1,10 +1,12 @@
+from functools import reduce
 from prosumer.mqtt import ProsumerMqttClient
 from prosumer.settings import PROFILE
 from prosumer.subsystem.base import SubsystemBase
 from prosumer.subsystem import Generation, Load, Storage
+from prosumer.subsystem.supports import SupportImportAndExport
 
 
-class Prosumer(SubsystemBase):
+class Prosumer(SubsystemBase, SupportImportAndExport):
     def __init__(
         self,
         vp_address: str = PROFILE["vp_address"],
@@ -24,4 +26,26 @@ class Prosumer(SubsystemBase):
             sys.start()
 
     def on_run(self):
-        print("running")
+        print(
+            f"total-gen={self.total_generation}KW  total-load={self.total_load}KW  net-gen={self.net_generation}KW  self-cons={self.self_consumption}KW"
+        )
+
+    @property
+    def total_generation(self) -> float:
+        return sum([sys.export_power for sys in self.generations])
+
+    @property
+    def total_load(self) -> float:
+        return sum([sys.import_power for sys in self.loads])
+
+    @property
+    def net_generation(self) -> float:
+        return self.total_generation - self.total_load
+
+    @property
+    def self_consumption(self) -> float:
+        return min(self.total_load, self.total_generation)
+
+    @property
+    def export_power(self) -> float:
+        return self.total_generation
